eba04c53fd6ea98dd2af94e348393181bfa2539a
Reyhan Memmedova <memmedova8m@gmail.com>
2022-12-14 03:23:39 +0400

    +fixes

--- 2/react-app/src/rout/login/LoginPage.jsx
+++ 2/react-app/src/rout/login/LoginPage.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react"
 
 export function Login(){
-    const[password,setPassword]=useState(0)
+    const[password,setPassword]=useState()
     return<><div style={{background:'grey',borderRadius: '50px',width:'400px',height:'400px',textAlign:'center',margin:'50px'}}>
     <input type="text" placeholder='username'style={{margin:'20px',borderRadius: '50px',height: "50px",textAlign:'center',color:'blue',borderColor:'orange'}}/>
     <br />
-    <input type="number" placeholder='password'style={{margin:'20px',borderRadius: '50px',height: "50px",textAlign:'center',borderColor:'orange'}} />
+    <input type="number" placeholder='password'style={{margin:'20px',borderRadius: '50px',height: "50px",textAlign:'center',borderColor:'orange'}} value={password} onChange={e => setPassword(parseInt(e.target.value))} />
     <br />
     <button style={{margin:'20px' ,backgroundColor: 'orange', borderRadius: '50px',width: "80px", height: "40px",textAlign:'center'}}>login</button>
     <br />
