df19fea5a880ab6393e2b7d4a23fb4b72cdb6cb3
Reyhan Memmedova <memmedova8m@gmail.com>
2022-12-14 23:40:17 +0400

    +fixed

--- 2/react-app/src/rout/login/LoginPage.jsx
+++ 2/react-app/src/rout/login/LoginPage.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react"
 
-export function Login(){
-    const[password,setPassword]=useState()
-    return<><div style={{background:'grey',borderRadius: '50px',width:'400px',height:'400px',textAlign:'center',margin:'50px'}}>
-    <input type="text" placeholder='username'style={{margin:'20px',borderRadius: '50px',height: "50px",textAlign:'center',color:'blue',borderColor:'orange'}}/>
-    <br />
-    <input type="number" placeholder='password'style={{margin:'20px',borderRadius: '50px',height: "50px",textAlign:'center',borderColor:'orange'}} value={password} onChange={e => setPassword(parseInt(e.target.value))} />
-    <br />
-    <button style={{margin:'20px' ,backgroundColor: 'orange', borderRadius: '50px',width: "80px", height: "40px",textAlign:'center'}}>login</button>
-    <br />
-    <div><input type="checkbox"style={{width:'50px',height:'20px',}}/>Remember me</div>
+export function Login() {
+    const [text, setText] = useState('')
+    const [password, setPassword] = useState()
+    const [checkbox, setCheckbox] = useState()
+    return <><div style={{ background: 'grey', borderRadius: '50px', width: '400px', height: '400px', textAlign: 'center', margin: '50px' }}>
+        <input type="text" placeholder='username' style={{ margin: '20px', borderRadius: '50px', height: "50px", textAlign: 'center', color: 'blue', borderColor: 'orange' }}value={text} onChange={e => setText((e.target.value))} />
+        <br />
+        <input type="password" placeholder='password' style={{ margin: '20px', borderRadius: '50px', height: "50px", textAlign: 'center', borderColor: 'orange' }} value={password} onChange={e => setPassword(parseInt(e.target.value))} />
+        <br />
+        <button style={{ margin: '20px', backgroundColor: 'orange', borderRadius: '50px', width: "80px", height: "40px", textAlign: 'center' }}>login</button>
+        <br />
+        <div><input type="checkbox" style={{ width: '50px', height: '20px', }} value={checkbox} onChange={e => setCheckbox((e.target.value))}/>Remember me</div>
     </div>
-      {/* <Routes>
+        {/* <Routes>
       <Route path=':/login' element={<Login></Login>} ></Route>
       </Routes> */}
-    </> 
+    </>
 }
