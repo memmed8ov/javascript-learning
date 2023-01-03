b65c3a6a753b0086a0f287ad485b3e5658e4c61d
Reyhan Memmedova <memmedova8m@gmail.com>
2023-01-03 21:51:52 +0400

    +fixes

--- 2/react-app/src/App.jsx
+++ 2/react-app/src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
-
+import { useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 
-import {Countries} from './rout/country/country-page';
+import { Countries } from './rout/country/country-page';
 
-import {Cities} from './rout/city/city-page';
-import{Login} from './rout/login/LoginPage';
+import { Cities } from './rout/city/city-page';
+import { Login } from './rout/login/LoginPage';
+
 
 export function NavBar() {
     const navigate = useNavigate()
+
+    const isLoggedIn = !!localStorage.getItem('token')
 
     return (<AppBar component="nav">
         <Toolbar>
@@ -26,18 +29,53 @@ export function NavBar() {
                 }}>
                     City
                 </Button>
-               
-                <Button sx={{ color: '#fff' }} onClick={() => {
-                    navigate('/LoginPage')
-                }}>
-                    LOGIN
-                </Button>
+                {isLoggedIn && <Button sx={{ color: '#fff' }} onClick={() => {
+                    localStorage.removeItem('token')
+                    navigate('/logout')
 
-                
+                }}>
+                    Logout
+                </Button>}
             </Box>
         </Toolbar>
     </AppBar>)
 }
+
+// eger login olmamisansa, Logout button gosterme
+
+function ProtectedRoutes() {
+    const navigate = useNavigate()
+
+    const isLoggedIn = !!localStorage.getItem('token')
+
+    console.log('step 1', isLoggedIn) // login oldugum halda isLoggedIn false dur, true olmali idi
+
+    useEffect(() => {
+        console.log('step 2', isLoggedIn)
+
+        if (!isLoggedIn) { // login oldugum halda, bu ifin icine girmemeli idi
+            console.log('step 3', isLoggedIn)
+            navigate('/login')
+            console.log('step 4', isLoggedIn)
+        }
+        console.log('step 5', isLoggedIn)
+    }, [isLoggedIn])
+
+    if (!isLoggedIn) {
+        return <></>
+    }
+
+    // ok[2:30, 2022-12-31] Enishte: 1. Eger login ugurlu oldusa, on sehifeye navigate ele ( / )
+    // ok[2:30, 2022-12-31] Enishte: 2. Eger localStorage de token adinda deyisen movcuddursa onda /login sehifesine redirect eleme
+
+    return <>
+        <Routes>
+            <Route path='/country-page/*' element={<Countries></Countries>} ></Route>
+            <Route path='/city-page/*' element={<Cities></Cities>} ></Route>
+        </Routes>
+    </>
+}
+
 
 function App() {
     return <>
@@ -47,10 +85,8 @@ function App() {
             <br />
             <br />
             <Routes>
-                <Route path='/country-page/*' element={<Countries></Countries>} ></Route>
-                <Route path='/city-page/*' element={<Cities></Cities>} ></Route>
-                <Route path='/LoginPage/*' element={<Login></Login>} ></Route>
-               
+                <Route path='/login/*' element={<Login></Login>} ></Route>
+                <Route path='/*' element={<ProtectedRoutes />} ></Route>
             </Routes>
         </Router>
     </>
--- 2/react-app/src/rout/login/LoginPage.jsx
+++ 2/react-app/src/rout/login/LoginPage.jsx
@@ -1,20 +1,91 @@
+import axios from "axios"
 import { useState } from "react"
-
+import { useNavigate } from 'react-router-dom'
 export function Login() {
+    const navigate = useNavigate()
     const [username, setUserName] = useState('')
     const [password, setPassword] = useState()
     const [rememberme, setRememberMe] = useState()
+    const [show, setShow] = useState('password')
     return <><div style={{ background: 'grey', borderRadius: '50px', width: '400px', height: '400px', textAlign: 'center', margin: '50px' }}>
-        <input type="text" placeholder='username' style={{ margin: '20px', borderRadius: '50px', height: "50px", textAlign: 'center', color: 'blue', borderColor: 'orange' }}value={username} onChange={e => setUserName((e.target.value))} />
+        <input type="text" placeholder='username' style={{ margin: '20px', borderRadius: '50px', height: "50px", textAlign: 'center', color: 'blue', borderColor: 'orange' }} value={username} onChange={e => setUserName((e.target.value))} />
         <br />
-        <input type="password" placeholder='password' style={{ margin: '20px', borderRadius: '50px', height: "50px", textAlign: 'center', borderColor: 'orange' }} value={password} onChange={e => setPassword(parseInt(e.target.value))} />
+        <input type={show} placeholder='password' style={{ margin: '20px', borderRadius: '50px', height: "50px", textAlign: 'center', borderColor: 'orange' }} value={password} onChange={e => setPassword((e.target.value))} />
+        <button onClick={() => {
+            if (show == 'password') {
+                setShow('text')
+            } else {
+                setShow('password')
+            }
+        }}
+            style={{
+                marginLeft: -50,
+                border: 0,
+                borderRadius: 38
+            }}>V</button>
         <br />
-        <button style={{ margin: '20px', backgroundColor: 'orange', borderRadius: '50px', width: "80px", height: "40px", textAlign: 'center' }}>login</button>
+        <button onClick={() => {
+
+            const isLoggedIn = localStorage.getItem('token')
+            if (isLoggedIn) {
+                navigate('/')
+            }
+
+            let object = {
+                "Username": username,
+                "Password": password
+            }
+            axios.post('http://tiswork.tisserv.net:8008/authentication/token', object).then((resp) => {
+                const token = resp.data.token.content
+                localStorage.setItem('token', token)
+            }, err => {
+                alert('Username ve ya password yanlishdir')
+
+            })
+
+
+        }} style={{ margin: '20px', backgroundColor: 'orange', borderRadius: '50px', width: "80px", height: "40px", textAlign: 'center' }}>login</button>
         <br />
-        <div><input type="checkbox" style={{ width: '50px', height: '20px', }} value={rememberme} onChange={e => setRememberMe((e.target.value))}/>Remember me</div>
+        <div><input type="checkbox" style={{ width: '50px', height: '20px', }} value={rememberme} onChange={e => setRememberMe((e.target.value))} />Remember me</div>
     </div>
         {/* <Routes>
       <Route path=':/login' element={<Login></Login>} ></Route>
       </Routes> */}
     </>
 }
+
+/* switch
+
+// light ON ; OFF
+
+knopkani basanda, on dusa off olmalidi, off dusa on olmalidi
+
+knopkani basanda:
+    ON  => OF
+    OFF => ON
+
+
+tip: PASSWORD; TEXT
+knopkani basanda:
+    PASSWORD => TEXT
+    TEXT => PASSWORD
+
+
+if (show == 'password') {
+    setShow('text')
+} else {
+    setShow('password')
+}
+
+*/
+
+// status code
+
+// 1** - Upgrade        -  no error
+// 2** - Success        -  no error
+// 3** - Redirect       - no error
+// 4** - Client error   - error      - Frontend error
+// 5** - Server error   - error      - Backend error
+
+
+
